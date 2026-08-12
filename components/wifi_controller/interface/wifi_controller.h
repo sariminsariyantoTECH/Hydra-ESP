8389e310d0cd41db45520c685ce44fcc4c91a288
sariminsariyantoTECH <sariminsariyanto@gmail.com>
2026-08-12 19:10:54 +0700

    Update wifi_controller.h

--- components/wifi_controller/interface/wifi_controller.h
+++ components/wifi_controller/interface/wifi_controller.h
@@ -16,79 +16,33 @@
 #include "../sniffer.h"
 
 #include "esp_wifi_types.h"
+#include "esp_err.h"
 
-/**
- * @brief Starts AP with given config
- * 
- * @param wifi_config 
- */
+// Variabel global untuk membaca channel AP yang aktif dari file lain
+extern uint8_t current_ap_channel;
+
+// --- Fungsi Asli Risinek / Hydraproject ---
 void wifictl_ap_start(wifi_config_t *wifi_config);
-
-/**
- * @brief Stops running AP
- * 
- */
 void wifictl_ap_stop();
-
-/**
- * @brief Starts default management AP
- * 
- */
 void wifictl_mgmt_ap_start();
-
-/**
- * @brief Connects station interface to the given AP
- *  
- * @param ap_record 
- * @param password password for target network
- */
 void wifictl_sta_connect_to_ap(const wifi_ap_record_t *ap_record, const char password[]);
-
-/**
- * @brief Disconnects station interface from currently connected AP
- * 
- */
 void wifictl_sta_disconnect();
-
-/**
- * @brief Sets AP interface MAC address
- * 
- * @param mac_ap valid MAC address is expected, array of size 6
- */
 void wifictl_set_ap_mac(const uint8_t *mac_ap);
-
-/**
- * @brief Saves current AP interface MAC to given parameter
- * 
- * @attention this function expects that the mac_ap points to already allocated memory (6 bytes)
- * @param mac_ap 6 bytes memory block
- */
 void wifictl_get_ap_mac(uint8_t *mac_ap);
-
-/**
- * @brief Restores original AP interface MAC that was set during Wi-Fi initialisation.
- */
 void wifictl_restore_ap_mac();
-
-/**
- * @brief Sets STA interface MAC address
- * 
- * @param mac_ap valid MAC address is expected, array of size 6
- */
 void wifictl_get_sta_mac(uint8_t *mac_sta);
-
-/**
- * @brief Sets new channel for Wi-Fi interface
- * 
- * @param channel channel in range 1 - 13
- */
 void wifictl_set_channel(uint8_t channel);
-
-/**
- * @brief Stops the management AP and resets the radio mode.
- */
 void wifictl_mgmt_ap_stop();
 
+// Read management AP credentials from NVS storage namespace "storage".
+// Outputs are written into provided buffers. Returns an esp_err_t.
+esp_err_t wifictl_get_mgmt_creds(char *out_ssid, size_t ssid_len, char *out_password, size_t password_len);
 
+// --- Fungsi Tambahan (Vendor Spoofing Dinamis) ---
+/**
+ * @brief Mengubah MAC Address AP agar menggunakan identitas vendor ZTE
+ *        dengan 3 byte terakhir yang diacak secara dinamis via hardware generator.
+ */
+void wifictl_set_vendor_zte_random_mac(void);
 
 #endif
